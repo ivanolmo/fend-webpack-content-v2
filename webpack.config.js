31f1d129872e155e908d2bb492b4b4787d23456b
Ivan Olmo <ivan.olmo@gmail.com>
2020-07-26 23:46:51 -0500

    Add HtmlWebpackPlugin to project. Minor related updates in other files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path")
 const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
 module.exports = {
     entry: './src/client/index.js',
     module: {
@@ -19,5 +21,11 @@ module.exports = {
                 loader: 'babel-loader'
             }
         ]
-    }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+          template: "./src/client/views/index.html",
+          filename: "./index.html"
+      })
+    ]
 }
