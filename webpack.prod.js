b5f5a9199b626b66b72f74604edd1ad9cc4e9c84
Ivan Olmo <ivan.olmo@gmail.com>
2020-07-27 00:43:50 -0500

    Add separate dev and prod config files, minor updates in related files

---
+++ webpack.prod.js
@@ -0,0 +1,32 @@
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: 'production',
+  entry: './src/client/index.js',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          cache: true
+        }
+      },
+      {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html"
+    })
+  ]
+}
