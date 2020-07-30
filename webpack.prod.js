70b17ae000360e94a5ab13423ada2f203323c5ca
Ivan Olmo <ivan.olmo@gmail.com>
2020-07-29 20:51:40 -0500

    Add service workers to project

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const common = require('./webpack.common.js');
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
@@ -20,6 +21,7 @@ module.exports = merge(common, {
   plugins: [
     new MiniCssExtractPlugin({
       filename: '[name].css'
-    })
+    }),
+    new WorkboxPlugin.GenerateSW()
   ]
 });
